4018b3c4fead46c36be4e008abdc5d11240895e8
solopku <solopku@hotmail.com>
2016-11-18 10:01:45 +0800

    Data Loader

---
+++ DataLoader.py
@@ -0,0 +1,26 @@
+import numpy as np
+from sklearn.preprocessing import OneHotEncoder
+
+def classificationDataLoader(fileName, labelCol=-1, delimiter=',', sparsify=False):
+    '''load classification data from fileName, transform into onehot feature representation X, and label Y
+        :param
+            fileName:   data file
+            labelCol:   column position for label, default last column
+            delimiter:  feature seperator each sample, default ','
+        :returns
+            X:  np feature matrix   (n_samples, n_features)
+            Y:  np label matrix     (n_samples, 1)
+    '''
+    data = np.loadtxt(fileName, dtype='int32', delimiter=',')
+    X, Y = data[:, xrange(data.shape[1]-1) if labelCol==-1 else xrange(1, data.shape[1])], data[:, labelCol]
+    #print type(OneHotEncoder().fit_transform(X))
+    return OneHotEncoder().fit_transform(X, ), Y if sparsify==True else OneHotEncoder().fit_transform(X, ).toarray(), Y
+
+
+# X, Y = classificationDataLoader('dataset/test.data')
+# print X.shape, Y.shape
+# print X, Y
+
+
+
+
--- Example-iris-smoothing.py
+++ Example-iris-smoothing.py
@@ -11,15 +11,20 @@ from sklearn.linear_model import ElasticNet, Lasso, RidgeClassifier
 from sklearn.grid_search import GridSearchCV
 from sklearn.cross_validation import StratifiedKFold, cross_val_score
 from sklearn.preprocessing import scale
+from DataLoader import classificationDataLoader
 
-data = load_iris()
+import warnings
+warnings.filterwarnings("ignore")
 
-# X = data['data']
-X = scale(data['data'])
-y = data['target']
+# data = load_iris()
+
+# # X = data['data']
+# X = scale(data['data'])
+# y = data['target']
+#
+X, y = classificationDataLoader('dataset/diabetic_data_categorical.csv', labelCol=0)
 print "X.shape = \n", X.shape
 print "y.shape = \n", y.shape
-print "data scaled!!\n"
 
 
 idx = np.random.permutation(X.shape[0])
@@ -37,13 +42,13 @@ ridge = RidgeClassifier(solver='lsqr')
 param_ridge = {'alpha': [100, 10, 1, 0.1, 1e-2, 1e-3]}
 
 huber = OneVsRestClassifier(HuberSVC())
-param_huber = {'estimator__C': [100, 10, 1, 0.1, 1e-2, 1e-3], 
+param_huber = {'estimator__C': [100, 10, 1, 0.1, 1e-2, 1e-3],
               'estimator__lambd': [100, 10, 1, 0.1, 1e-2, 1e-3], 
               'estimator__mu': [100, 10, 1, 0.1, 1e-2, 1e-3]}
 
 smoothing = OneVsRestClassifier(Smoothing_Regularization())
 param_smoothing = {'estimator__C': [100, 10, 1, 0.1, 1e-2, 1e-3],
-                   'estimator__alpha': [100, 10, 1, 0.1, 1e-2, 1e-3]}
+                   'estimator__alpha': [10000, 1000, 100, 10, 1, 0.1, 1e-2, 1e-3, 1e-4]}
 
 n_folds = 5
 param_folds = 3
@@ -51,7 +56,7 @@ scoring = 'accuracy'
 
 result_df = pandas.DataFrame()
 for i, (train_index, test_index) in enumerate(StratifiedKFold(y, n_folds=n_folds)):
-    for clf_name, clf, param_grid in [('Smoothing_Regularization', smoothing, param_smoothing), 
+    for clf_name, clf, param_grid in [#('Smoothing_Regularization', smoothing, param_smoothing),
                                       ('ElasticNet', elastic, param_elastic), 
                                       ('Ridge', ridge, param_ridge), 
                                       # ('HuberSVC', huber, param_huber),
