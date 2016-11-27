d18a3460b29818558cf9dc55954284f158d45d60
zhaojing <zhaojing@comp.nus.edu.sg>
2016-11-28 00:13:01 +0800

    svmlight loader

--- smoothing_regularization.py
+++ smoothing_regularization.py
@@ -7,7 +7,7 @@ from sklearn.linear_model.base import LinearClassifierMixin, BaseEstimator
 import random
 
 def smoothing_grad_descent(X, y, w, param, C, batch_size, batch_iter):
-    # ?? sparse matrix works? random.shuffle?
+    # sparse matrix works, random.shuffle
     # shuffle: next time shuffle index will be forgetten (static variable: smoothing_grad_descent.idx)
     if not hasattr(smoothing_grad_descent, "idx"):
         smoothing_grad_descent.idx = np.random.permutation(X.shape[0])
@@ -15,12 +15,12 @@ def smoothing_grad_descent(X, y, w, param, C, batch_size, batch_iter):
     y = y[smoothing_grad_descent.idx]
     index = (batch_size * batch_iter) % X.shape[0]
     if (index + batch_size) > X.shape[0]: #new epoch
-        batch_X, batch_y = X[0 : batch_size], y[0: batch_size]
+        batch_X, batch_y = X[0 : batch_size], y[0: batch_size] #fixed shape
         batch_X[0 : (X.shape[0] - index)], batch_y[0 : (X.shape[0] - index)] = X[index : X.shape[0]], y[index : X.shape[0]]
         batch_X[(X.shape[0] - index) : batch_size], batch_y[(X.shape[0] - index) : batch_size] = X[0 : (index + batch_size - X.shape[0])], y[0 : (index + batch_size - X.shape[0])]
-        # trt print the idx?
+        # trt print the idx
         smoothing_grad_descent.idx = np.random.permutation(X.shape[0])
-    else: # need else here?
+    else: # need else here
         batch_X, batch_y = X[index : (index + batch_size)], y[index : (index + batch_size)]
     
     if sparse.issparse(batch_X): batch_X = batch_X.toarray()
@@ -40,18 +40,19 @@ def smoothing_optimizator(X, y, lambd, C, max_iter, eps, alpha, decay, batch_siz
     
     batch_iter = 0
     while True:
-        batch_iter = batch_iter + 1
         w_update = alpha * smoothing_grad_descent(X, y, w, lambd, C, batch_size, batch_iter)
         w -= w_update
         alpha -= alpha * decay
         k += 1
+        print "smoothing_optimizator k: ", k
+        batch_iter = batch_iter + 1
         if k >= max_iter or np.linalg.norm(w_update, ord=2) < eps:
             break
     return k, w
 
 class Smoothing_Regularization(BaseEstimator, LinearClassifierMixin):
 
-    def __init__(self, C=1.,lambd=1., max_iter=1000, eps=0.0001, alpha=0.01, decay=0.01, fit_intercept=True, batch_size=20):
+    def __init__(self, C=1.,lambd=1., max_iter=1000, eps=0.0001, alpha=0.01, decay=0.01, fit_intercept=True, batch_size=100):
         self.C = C
         self.lambd = lambd
         self.max_iter = max_iter
