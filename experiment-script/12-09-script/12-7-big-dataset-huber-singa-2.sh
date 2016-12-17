d5266b61de4951c9df64bdb5e16a1587b1bba431
zhaojing <zhaojing@comp.nus.edu.sg>
2016-12-17 23:14:32 +0800

    experiment script

---
+++ experiment-script/12-09-script/12-7-big-dataset-huber-singa-2.sh
@@ -0,0 +1,2 @@
+python /home/singa/zhaojing/smooth-regularization/smoothing-regularization/Example-iris-smoothing.py -datapath /data/zhaojing/regularization/url-dataset/url_svmlight_merge/Day0.svm -labelcolumn 1 -svmlight 1 -sparsify 1 -scale 0 | tee -a /data/zhaojing/regularization/log1209/Day0cale0.log
+python /home/singa/zhaojing/smooth-regularization/smoothing-regularization/Example-iris-smoothing.py -datapath /data/zhaojing/regularization/url-dataset/url_svmlight_merge/Day0.svm -labelcolumn 1 -svmlight 1 -sparsify 1 -scale 0 | tee -a /data/zhaojing/regularization/log1209/Day0cale0.log
