504e0a29160ed37691b0e0861d55d4e1f6323f9c
zhaojing <zhaojing@comp.nus.edu.sg>
2017-03-30 00:00:27 +0800

    svm file read in

--- data_loader.py
+++ data_loader.py
@@ -5,24 +5,37 @@ from gm_prior_simulation import Simulator
 from pickle_transformer import Dataset
 from scipy import sparse
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.datasets import load_svmlight_file
 
 # load training/testing data from pickles simulator object file with specified training percent
 def loadData(fileName, onehot=True, sparsify=True):
-    print '\n===============================================\n'
-    print 'loading data...'
-    pklfile = pickle.load(open(fileName, 'r'))
-    X, Y = pklfile.x, pklfile.label.reshape((pklfile.sample_num, 1))
-    print 'finish loading data...\ndata samples %d' %(pklfile.sample_num)
-    print 'data dimension %d' %(pklfile.dimension)
-    print '\n===============================================\n'
-    print "X shape: ", X.shape
-    print "Y shape: ", Y.shape
-    print "Y[:10] before transform: ", Y[:10]
-    if Y.dtype != 'bool':
-        Y = (Y > 0.5)
-    print "Y[:10] after transform: ", Y[:10]
-    # return X, Y
-    if onehot:
-        return (OneHotEncoder().fit_transform(X, ), sparse.csr_matrix(Y)) if sparsify==True else (OneHotEncoder().fit_transform(X, ).toarray(), Y)
+    if 'pkl' in fileName:
+        print '\n===============================================\n'
+        print 'loading pkl data...'
+        pklfile = pickle.load(open(fileName, 'r'))
+        X, Y = pklfile.x, pklfile.label.reshape((pklfile.sample_num, 1))
+        print 'finish loading data...\ndata samples %d' %(pklfile.sample_num)
+        print 'data dimension %d' %(pklfile.dimension)
+        print '\n===============================================\n'
+        print "X shape: ", X.shape
+        print "Y shape: ", Y.shape
+        print "Y[:10] before transform: ", Y[:10]
+        if Y.dtype != 'bool':
+            Y = (Y > 0.5)
+        print "Y[:10] after transform: ", Y[:10]
+        # return X, Y
+        if onehot:
+            return (OneHotEncoder().fit_transform(X, ), sparse.csr_matrix(Y)) if sparsify==True else (OneHotEncoder().fit_transform(X, ).toarray(), Y)
+        else:
+            return (sparse.csr_matrix(X), sparse.csr_matrix(Y)) if sparsify==True else (X, Y)
     else:
-        return (sparse.csr_matrix(X), sparse.csr_matrix(Y)) if sparsify==True else (X, Y)
+        print '\n===============================================\n'
+        print 'loading svm data...'
+        data = load_svmlight_file(fileName)
+        X, Y = data[0], data[1]
+        print "svmlight X shape: ", X.shape
+        print "svmlight Y shape: ", Y.shape
+        Y = (Y > 0.5)
+        Y = Y.reshape((-1, 1))
+        print "Y[:10] after transform: ", Y[:10]
+        return X, sparse.csr_matrix(Y)
