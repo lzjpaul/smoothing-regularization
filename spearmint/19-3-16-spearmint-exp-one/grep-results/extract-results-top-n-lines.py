0b72ee810e7e33d3754c7df7efd17d08a30de887
zhaojing <zhaojingluo@gmail.com>
2019-03-25 11:54:23 +0800

    extract results python

--- spearmint/19-3-16-spearmint-exp-one/grep-results/extract-results-top-n-lines.py
+++ spearmint/19-3-16-spearmint-exp-one/grep-results/extract-results-top-n-lines.py
@@ -1,9 +1,11 @@
+# for conn sonar, needs to restrict to first 80 lines
 import pandas
 import argparse
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Extract Results')
     parser.add_argument('-resultspath', type=str, help='the results path')
+    parser.add_argument('-topn', type=int, help='top n lines')
     args = parser.parse_args()
 
     print 'resultspath: ', args.resultspath
@@ -21,7 +23,7 @@ if __name__ == '__main__':
     accuracy_df.loc[3, (str(resultslineList[0].split()[0]))] = float(resultslineList[4].split()[1])
     accuracy_df.loc[4, (str(resultslineList[0].split()[0]))] = float(resultslineList[5].split()[1])
 
-    for i in range(len(resultslineList)):
+    for i in range(args.topn):
         if i >= 7:
             if (i-7)%8 == 0:
                 accuracy_df.loc[0, (str(resultslineList[i+1].split()[0]))] = float(resultslineList[i+2].split()[1])
@@ -34,4 +36,5 @@ if __name__ == '__main__':
 
     print("accuracy best subsample %0.6f (+/-%0.06f)"
                          % (accuracy_df.max(axis=1).mean(), accuracy_df.max(axis=1).std()))
-# python extract-results.py -resultspath 19-3-16-spearmint-exp-one-results-7
+
+# python extract-results-top-n-lines.py -resultspath 19-3-16-spearmint-exp-one-results-7 -topn 100
